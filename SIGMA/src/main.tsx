4b0b82a3e081230e92111abbe0d41cb18b8d0de8
Mity619 <iamsacul7@gmail.com>
2026-05-23 22:07:13 -0500

    Implementado Alertas Notistack En SignIn y SignUp

--- SIGMA/src/main.tsx
+++ SIGMA/src/main.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import  {AuthContextProvider } from "./Context/AuthContext"
 import App from './App';
 import './index.css';
+import { SnackbarProvider } from 'notistack';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
     <BrowserRouter>
         <AuthContextProvider>
-            <App />
+            <SnackbarProvider maxSnack={3}>
+                <App />
+            </SnackbarProvider>
         </AuthContextProvider>       
     </BrowserRouter>
 
