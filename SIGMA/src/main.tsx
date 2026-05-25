7ae6309c128b07b5983b1455d662f47b4b37563e
Laura <laucamp4284@gmail.com>
2026-05-24 21:14:38 -0500

    Añadir notiStack a arboles y grafos

--- SIGMA/src/main.tsx
+++ SIGMA/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';  // ✅ Importa correctamente
+import { SnackbarProvider } from 'notistack';
 import { BrowserRouter } from 'react-router-dom';
 import  {AuthContextProvider } from "./Context/AuthContext"
 import App from './App';
@@ -9,7 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
     <BrowserRouter>
         <AuthContextProvider>
-            <App />
+            <SnackbarProvider maxSnack={3}>
+                <App />
+            </SnackbarProvider>
         </AuthContextProvider>       
     </BrowserRouter>
 
